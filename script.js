006a85e9967ddf79b606b676e812c9bb6a360aa9
gaeun222 <srehke123@gmail.com>
2025-05-13 18:15:39 +0900

    0513_1

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-AOS.init({
-    // once: true,
+$(".dropdown-toggle").click(function (e) {
+    e.stopPropagation();
+
+    // 모든 드롭다운 닫고, 아이콘 원복
+    $(".dropdown-menu").not($(this).next()).slideUp(200);
+    $(".arrow-icon").not($(this).find(".arrow-icon")).removeClass("arrow-rotated");
+
+    // 현재 메뉴 토글
+    $(this).next(".dropdown-menu").slideToggle(200);
+
+    // 이미지 회전 토글
+    $(this).find(".arrow-icon").toggleClass("arrow-rotated");
   });
 
-  $(".que").click(function() {
-    $(this).next(".anw").stop().slideToggle(300);
-   $(this).toggleClass('on').siblings().removeClass('on');
-   $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
- });
+  $(document).click(function () {
+    $(".dropdown-menu").slideUp(200);
+    $(".arrow-icon").removeClass("arrow-rotated");
+  });
+
+  $(".dropdown-menu").click(function (e) {
+    e.stopPropagation();
+  });
+
+AOS.init({
+    once: true,
+  });
+
+$(document).ready(function() {
+  $(".anw").hide(); // 모든 답변 숨기기
+
+  $(".que").click(function () {
+    let $nextItems = $(this).nextUntil(".que", ".anw");
+    $(this).toggleClass('on');
+    $nextItems.stop().slideToggle(300);
+  });
+});
